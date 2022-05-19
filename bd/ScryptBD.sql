35dd9eade65e220e9665f1004d06d5638812dcfd
Qquehue <alejandro.qquehue@sptech.school>
2022-05-19 08:50:25 -0300

    atualização

--- bd/ScryptBD.sql
+++ bd/ScryptBD.sql
@@ -1,41 +1,50 @@
-create database teste;
-use teste;
+create database ShadowRealms;
+use ShadowRealms;
 
 create table Usuario(
 idUsuario int primary key auto_increment,
 nome varchar(45),
+email varchar(45),
 tipo varchar(11),
-descricao varchar(100)
+descricao text
 );
 create table Forum(
 idDiscussao int primary key auto_increment,
-Discussao varchar(800),
+titulo varchar (45),
+Discussao text,
 statusDisc char(7),
+tipo varchar(45),
+URL varchar(100),
 fkUsuario int,
+registroDisc datetime default current_timestamp,
 foreign key (fkUsuario) references Usuario(idUsuario)
 );
 create table respostas (
 idResposta int auto_increment,
-fkDiscussao int,
-foreign key (fkDiscussao) references Forum(idDiscussao),
-primary key (idResposta,fkDiscussao),
-resposta varchar(800)
+fkUsuario int,
+foreign key (fkUsuario) references Usuario (idUsuario),
+fkForum int,
+foreign key (fkForum) references Forum (idDiscussao),
+primary key (idResposta, fkUsuario, fkForum),
+registro datetime default current_timestamp,
+descricao text
 );
 
 insert into Usuario values
-(null,'GalinhoChickerLittle123', 'competitivo', 'eu gosto de cartinhas maneiras'),
-(null, 'DarkGuy1', 'fã do anime', 'yugi moto maior de todos, vai dark magician'),
-(null, 'Manito', 'casual', 'devoto ao pendulum summon');
+(null,'DarkNight','marquinhos@email.com','Casual','Jogador Casual, Rogue Decks'),
+(null,'YugiGuy','souza123@email.com','fã do anime','O Mago definitivo em termos de ataque e defesa'),
+(null,'bestDino','SilvaVictor@email.com','competitivo','Eu amo ler cartinhas');
 
-insert into forum values
-(null,'Duvida minha sobre a pendulum summon, por que ela existe???','aberto',1),
-(null,'Alguém me explica a regra de prioridade por favor?','fechado',2),
-(null,'Odiei a Banlist mano','aberto',1);
+insert into forum (titulo,discussao,statusDisc,tipo,URL,fkUsuario)values
+('odeio pendulum summon', 'Acho que o fato deu eu odiar a pendulum summon é evidente, só quero saber que compartilha dessa ideia','aberto',
+'texto','none',1);
+insert into forum (titulo,discussao,statusDisc,tipo,URL,fkUsuario)values
+('duvida sobre prioridade', 'queria saber sobre como funciona a questão da prioridade no jogo','aberto',
+'texto','none',1),
+('amo dark magician', 'alguém poderia me mandar um deck build de Mago Negro por favor?','aberto','texto','none',2),
+('missprint', 'olha a missprint que eu tirei hahahaha','fechado','imagem','arquivos/imagens/missprint',3);
 
-insert into respostas values
-(null, 1, 'Mano eu também não sei velho, gatilho d+, bagulho debalanceado, deve ser pra poder cobrar caro nos cards'),
-(null, 2, 'Basicamente se trata de uma ação sua poder se impor em situações espicificas, como um summon, existem monstros que se ativam a tal ação');
-
-select * from usuario;
-select * from forum;
-select * from respostas;
+insert into respostas (fkUsuario, fkForum, descricao) values
+(1,2,'gente queria deixar claro que me refiro mais em casos de negações de summon'),
+(2,2,'oi tudo bem? a prioridade acontece uma vez que você cumpre uma ação, se a summon for negada,você nem chega a cumprir a condição para poder ativar o efeito em questão'),
+(3,3,'segue esse link https//:decklist.com');
